e8138addf56af00541f8030552baf6dce1fb6f7a
Michael Pereira <michael.pereira@chat2desk.com.br>
2022-11-06 17:22:59 -0300

    feat: add event in port

--- portas/src/components/Porta.tsx
+++ portas/src/components/Porta.tsx
@@ -3,18 +3,32 @@ import PortaModel from '../model/porta'
 
 interface PortaProps {
   value: PortaModel;
+  onChange: (novaPorta: PortaModel) => void;
 }
 
 export default function Porta(props:PortaProps){
   const porta =  props.value
-  const selecionada = porta.selecionada ? styles.selecionada : ''
-  return (
-    <div className={styles.area}>
-      <div className={`${styles.estrutura} ${selecionada}`}>
+
+  const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : ''
+
+  const alternarSelecao = e => props.onChange(porta.alternarSelecao())
+
+  const abrir = e => {
+    e.stopPropagation()
+    props.onChange(porta.abrir())
+  }
+  function renderizarPorta(){
+    return (
         <div className={styles.porta}>
           <div className={styles.numero}>{porta.numero}</div>
-          <div className={styles.macaneta}></div>
+          <div className={styles.macaneta} onClick={abrir}></div>
         </div>
+    )
+  }
+  return (
+    <div className={styles.area} onClick={alternarSelecao}>
+      <div className={`${styles.estrutura} ${selecionada}`}>
+        {porta.aberta ? false : renderizarPorta()}
       </div>
       <div className={styles.chao}>
 
